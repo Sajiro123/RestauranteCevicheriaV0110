c327a7f8cc5ac5cd160af97cabc2f9a94e4f266b
Sajiro123 <37230179+Sajiro123@users.noreply.github.com>
2025-10-01 00:31:21 -0500

    Start repository

---
+++ src/app/model/NuevoPedido.ts
@@ -0,0 +1,23 @@
+import { NuevoPedidodetalle } from './NuevoPedidodetalle';
+
+export interface NuevoPedido {
+    idpedido: number;
+    lugarpedido: any;
+    pedido_estado: any;
+    nombre: any;
+    cantidad: number;
+    descripcion: string;
+    estado: boolean;
+    lugar: string;
+    preciounitario: number;
+    total: number;
+    descuento: number;
+    comentario: string;
+    pedidodetalle: NuevoPedidodetalle[];
+    visa: number;
+    yape: number;
+    plin: number;
+    efectivo: number;
+    cliente?: string | null;
+    delivery?: number | null;
+}
