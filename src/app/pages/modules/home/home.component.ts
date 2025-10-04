61c7abd33904539869694d48f9eb52b94b311104
Sajiro123 <37230179+Sajiro123@users.noreply.github.com>
2025-10-04 18:47:42 -0500

    Updated home.component.ts

--- src/app/pages/modules/home/home.component.ts
+++ src/app/pages/modules/home/home.component.ts
@@ -485,7 +485,7 @@ export class HomeComponent {
     try {
         // 1️⃣ Preparamos los datos
         const pedido = this.NuevoPedido;
-        this.nuevoPedido.comentario= this.comentarios;
+        this.NuevoPedido.comentario= this.comentarios;
         const detalles = this.NuevoPedido.pedidodetalle.map((element) => ({
             ...element,
             idpedido: pedido.idpedido,
